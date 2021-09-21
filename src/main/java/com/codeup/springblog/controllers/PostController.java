9364ee0fc942fa6d8e86ef61bc5434378c96805b
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-09-21 12:21:21 -0500

    finished assignment

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.codeup.springblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -27,9 +28,9 @@ public class PostController {
         return "view form for creating post";
     }
 
-    @GetMapping("/posts/create")
+    @PostMapping("/posts/create")
     @ResponseBody
     public String newPost(){
-        return "posts index page";
+        return "create new post";
     }
 }
