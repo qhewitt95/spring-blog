9a2942cbc92ee261ab6e664a27b1a83aa70cfafb
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-10-07 14:14:18 -0500

    exercise

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -22,6 +22,7 @@ public class RollDiceController {
         model.addAttribute("diceRoll", random);
         model.addAttribute("userGuess", n);
         model.addAttribute("isCorrect", random == n);
+
         return "roll-dice";
 
     }
